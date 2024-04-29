ec90593ac79ac9a65140641f2fb241941f466d67
Eduardo Alejandro Martínez Villalobos <eduardoalejandro.v.mtz@gmail.com>
2024-04-29 00:48:06 -0600

    translate spanish to english

--- Actvidad 2 S4/MainWindow.xaml.cs
+++ Actvidad 2 S4/MainWindow.xaml.cs
@@ -6,39 +6,38 @@ namespace SequentialFileOperations
 {
     public partial class MainWindow : Window
     {
-        // Ruta del archivo secuencial
+        // Path to the file
         private const string filePath = "sequentialFile.txt";
 
         public MainWindow()
         {
             InitializeComponent();
         }
-
-        // Evento para escribir en el archivo secuencial
+        //Event to write in the sequential fil
         private void btnWrite_Click(object sender, RoutedEventArgs e)
         {
-            // Obtiene el texto del TextBox
+            // Get the text to write
             string textToWrite = txtInput.Text;
 
-            // Abre el archivo en modo de anexar (append), y escribe el texto
+            //Open the file in mode append, and write the text
             using (StreamWriter writer = new StreamWriter(filePath, true))
             {
                 writer.WriteLine(textToWrite);
             }
 
-            // Limpia el TextBox
+            // Clear textbox
             txtInput.Clear();
 
             txtOutput.Text = "Text written to file.";
         }
 
-        // Evento para leer desde el archivo secuencial
+        // Event to delete the file
         private void btnRead_Click(object sender, RoutedEventArgs e)
         {
-            // Verifica si el archivo existe
+            // Verify if the file exists
             if (File.Exists(filePath))
             {
-                // Lee todo el contenido del archivo
+                // Read all text from the file
                 using (StreamReader reader = new StreamReader(filePath))
                 {
                     string content = reader.ReadToEnd();
@@ -51,13 +50,13 @@ namespace SequentialFileOperations
             }
         }
 
-        // Evento para eliminar el archivo secuencial
+        // Event to delete the file
         private void btnDelete_Click(object sender, RoutedEventArgs e)
         {
-            // Verifica si el archivo existe
+            // Verify if the file exists
             if (File.Exists(filePath))
             {
-                // Elimina el archivo
+                // Delete the file
                 File.Delete(filePath);
                 txtOutput.Text = "File deleted.";
             }
